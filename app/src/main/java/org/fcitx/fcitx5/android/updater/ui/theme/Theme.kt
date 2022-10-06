ffd3c4f56a0f97b6fd845e32569d29495c67bd73
Rocka <i@rocka.me>
2022-10-06 23:24:25 +0800

    Proper dark theme

--- app/src/main/java/org/fcitx/fcitx5/android/updater/ui/theme/Theme.kt
+++ app/src/main/java/org/fcitx/fcitx5/android/updater/ui/theme/Theme.kt
@@ -2,6 +2,8 @@ package org.fcitx.fcitx5.android.updater.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
+import androidx.compose.material.Typography
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
@@ -16,15 +18,6 @@ private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
     secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
 
 @Composable
@@ -39,8 +32,6 @@ fun Fcitx5ForAndroidUpdaterTheme(
     }
     MaterialTheme(
         colors = colors,
-        typography = Typography,
-        shapes = Shapes,
         content = content
     )
 }
