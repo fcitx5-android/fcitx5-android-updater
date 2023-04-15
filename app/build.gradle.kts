1fad0b9b31b5b5d6ed0b3ff36769f00d123b1533
Rocka <i@rocka.me>
2023-04-15 21:07:54 +0800

    Downgrade kotlin and compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.5"
+        kotlinCompilerExtensionVersion = "1.4.4"
     }
 }
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "7.4.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
 
 tasks.register("clean", Delete::class) {
