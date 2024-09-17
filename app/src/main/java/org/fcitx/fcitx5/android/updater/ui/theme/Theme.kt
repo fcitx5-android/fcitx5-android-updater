3e38c2f60de5f2b549003c141b8d8a56171fa002
Rocka <i@rocka.me>
2024-09-17 17:38:44 +0800

    Refactor theme typography and shapes

--- app/src/main/java/org/fcitx/fcitx5/android/updater/ui/theme/Theme.kt
+++ app/src/main/java/org/fcitx/fcitx5/android/updater/ui/theme/Theme.kt
@@ -1,21 +1,50 @@
 package org.fcitx.fcitx5.android.updater.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
+import androidx.compose.material.Typography
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-private val DarkColorPalette = darkColors(
+private val DarkColors = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
     secondary = Teal200
 )
 
-private val LightColorPalette = lightColors(
+private val LightColors = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
     secondary = Teal200
+)
+
+val DenseTypography = Typography(
+    body1 = TextStyle(
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Normal
+    ),
+    body2 = TextStyle(
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Normal
+    ),
+    button = TextStyle(
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.sp
+    )
+)
+
+private val SharpShapes = Shapes(
+    small = RoundedCornerShape(2.dp),
+    medium = RoundedCornerShape(2.dp),
+    large = RoundedCornerShape(0.dp),
 )
 
 @Composable
@@ -23,14 +52,10 @@ fun Fcitx5ForAndroidUpdaterTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
     MaterialTheme(
-        colors = colors,
-        typography = Typography,
+        colors = if (darkTheme) DarkColors else LightColors,
+        typography = DenseTypography,
+        shapes = SharpShapes,
         content = content
     )
 }
